507ebce0520322f59b631618898c33ce388df77a
chirag-brillianklink <chirag@brilliantlink.tech>
2024-08-12 15:49:10 +0530

    added redirects for slug pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/defensive",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/offensive",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
